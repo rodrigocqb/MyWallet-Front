4f524c8f93e7d7370416aeee5aca8f3a469aee37
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-06 12:40:42 -0300

    Created Global Style

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import GlobalStyle from "../style/globalStyle";
 import Login from "./Login";
 import SignUp from "./SignUp";
 import Statement from "./Statement";
@@ -7,6 +8,7 @@ import Transaction from "./Transaction";
 function App() {
   return (
     <BrowserRouter>
+      <GlobalStyle />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/sign-up" element={<SignUp />} />
