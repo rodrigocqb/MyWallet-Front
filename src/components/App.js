85124753fd927d35c4953ce1ebceb4b3c4e8d097
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-06 12:02:45 -0300

    Created routes using react-router-dom

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./Login";
+import SignUp from "./SignUp";
+import Statement from "./Statement";
+import Transaction from "./Transaction";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/statement" element={<Statement />} />
+        <Route path="/new-transaction" element={<Transaction />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
