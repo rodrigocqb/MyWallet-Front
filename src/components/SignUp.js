be42bef292584e975d50af35eb9fe93a27ca2837
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-06 16:21:00 -0300

    Sign Up page layout done

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,3 +1,20 @@
 export default function SignUp() {
-  return <></>;
+  return (
+    <main>
+      <Title>MyWallet</Title>
+      <Form>
+        <input placeholder="Nome" type="text" name="nome" required />
+        <input placeholder="E-mail" type="email" name="email" required />
+        <input placeholder="Senha" type="password" name="password" required />
+        <input
+          placeholder="Confirme a senha"
+          type="password"
+          name="password"
+          required
+        />
+        <button>Cadastrar</button>
+      </Form>
+      <Span>Já tem uma conta? Entre agora!</Span>
+    </main>
+  );
 }
