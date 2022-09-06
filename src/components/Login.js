5f8977ab253438b01a252868b83fed653e65496d
rodrigocqb <rodrigocortibarros@gmail.com>
2022-09-06 18:05:54 -0300

    Mocking up login/sign up while back-end is not done

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,25 +1,60 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import styled from "styled-components";
 import { Form } from "../common/Form";
 import { LanguageSelect } from "./LanguageSelect";
 
 export default function Login() {
+  const [form, setForm] = useState({ email: "", password: "" });
+
   const { t } = useTranslation();
+
+  const navigate = useNavigate();
+
+  function handleForm({ value, name }) {
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(form);
+    navigate("/statement");
+  }
 
   return (
     <main>
       <Title>MyWallet</Title>
       <LanguageSelect />
-      <Form>
-        <input placeholder="E-mail" type="email" name="email" required />
+      <Form onSubmit={handleSubmit}>
+        <input
+          placeholder="E-mail"
+          type="email"
+          name="email"
+          onChange={(e) =>
+            handleForm({
+              value: e.target.value,
+              name: e.target.name,
+            })
+          }
+          required
+        />
         <input
           placeholder={t("password")}
           type="password"
           name="password"
+          onChange={(e) =>
+            handleForm({
+              value: e.target.value,
+              name: e.target.name,
+            })
+          }
           required
         />
-        <button>{t("enter")}</button>
+        <button type="submit">{t("enter")}</button>
       </Form>
       <Link to="/sign-up">
         <Span>{t("signUpLink")}</Span>
--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,32 +1,89 @@
+import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Form } from "../common/Form";
 import { LanguageSelect } from "./LanguageSelect";
 import { Span, Title } from "./Login";
 
 export default function SignUp() {
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+
   const { t } = useTranslation();
+
+  const navigate = useNavigate();
+
+  function handleForm({ value, name }) {
+    setForm({
+      ...form,
+      [name]: value,
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(form);
+    navigate("/login");
+  }
 
   return (
     <main>
       <Title>MyWallet</Title>
       <LanguageSelect />
-      <Form>
-        <input placeholder={t("name")} type="text" name="name" required />
-        <input placeholder="E-mail" type="email" name="email" required />
+      <Form onSubmit={handleSubmit}>
+        <input
+          placeholder={t("name")}
+          type="text"
+          name="name"
+          onChange={(e) =>
+            handleForm({
+              value: e.target.value,
+              name: e.target.name,
+            })
+          }
+          required
+        />
+        <input
+          placeholder="E-mail"
+          type="email"
+          name="email"
+          onChange={(e) =>
+            handleForm({
+              value: e.target.value,
+              name: e.target.name,
+            })
+          }
+          required
+        />
         <input
           placeholder={t("password")}
           type="password"
           name="password"
+          onChange={(e) =>
+            handleForm({
+              value: e.target.value,
+              name: e.target.name,
+            })
+          }
           required
         />
         <input
           placeholder={t("confirmPassword")}
           type="password"
-          name="password"
+          name="confirmPassword"
+          onChange={(e) =>
+            handleForm({
+              value: e.target.value,
+              name: e.target.name,
+            })
+          }
           required
         />
-        <button>{t("signUp")}</button>
+        <button type="submit">{t("signUp")}</button>
       </Form>
       <Link to="/">
         <Span>{t("login")}</Span>
